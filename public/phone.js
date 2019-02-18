a7e9fa95a0f52b0bf20a6d1ef973b7ae867714eb
Ethan Witherington <wyddindustries@gmail.com>
2019-02-18 16:00:32 -0500

    fixed vulnerability report

--- public/phone.js
+++ public/phone.js
@@ -6,7 +6,7 @@ let content = document.getElementById('content');
 
 // ~~~~~ Establish Network and Define Reactions to Events ~~~~~
 //HEY SERVER! Here I am :)
-var socket = io.connect()
+var socket = io.connect();
 //Server said hi!
 socket.on('connect',function(){
     console.log('Connected! ID is'+socket.io.engine.id);
